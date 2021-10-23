dc606d160e68a1208732c258045a3783e0ffcb4b
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-10-24 03:49:52 +0500

    basic app complete

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -9,84 +9,37 @@ class XylophoneApp extends StatelessWidget {
     player.play('not$num.wav');
   }
 
+  Expanded playbutton(Color color, int number) {
+    return Expanded(
+      child: FlatButton(
+        child: Text(
+          '',
+          style: TextStyle(fontSize: 0, color: Colors.black),
+        ),
+        color: color,
+        onPressed: () {
+          playnote(number);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               //For some reason TextButton not working here but flatbutton works perfect.
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.blueAccent,
-                onPressed: () {
-                  playnote(1);
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.greenAccent,
-                onPressed: () {
-                  playnote(2);
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.redAccent,
-                onPressed: () {
-                  playnote(3);
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.yellowAccent,
-                onPressed: () {
-                  playnote(4);
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.blueAccent,
-                onPressed: () {
-                  playnote(5);
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.purpleAccent,
-                onPressed: () {
-                  playnote(6);
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Play tone',
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
-                color: Colors.tealAccent,
-                onPressed: () {
-                  playnote(7);
-                },
-              ),
+              playbutton(Colors.red, 1),
+              playbutton(Colors.greenAccent, 2),
+              playbutton(Colors.blue, 3),
+              playbutton(Colors.grey, 4),
+              playbutton(Colors.yellow, 5),
+              playbutton(Colors.pinkAccent, 6),
+              playbutton(Colors.deepPurple, 7),
             ],
           ),
         ),
