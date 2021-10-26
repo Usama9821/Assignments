7ee6863e4cdf83009243312a5ec8b23b19f936ff
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-10-26 10:41:22 +0500

    added app icon

--- xylophone_app/lib/xylo.dart
+++ xylophone_app/lib/xylo.dart
@@ -26,6 +26,7 @@ class xylo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black87,
