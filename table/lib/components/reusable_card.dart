326af8542582d3b336327547eb728f3daa3b3d43
Rao Muhammad Usama <70236162+Usama9821@users.noreply.github.com>
2021-11-11 09:14:29 +0500

    .

--- table/lib/components/reusable_card.dart
+++ table/lib/components/reusable_card.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild, this.onPress});
+  ReusableCard(
+      {required this.colour, required this.cardChild, required this.onPress});
 
   final Color colour;
   final Widget cardChild;
